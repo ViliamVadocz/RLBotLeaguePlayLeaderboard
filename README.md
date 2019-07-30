b521f00f21fb167f045a73c43ee75561b1ff009f
ViliamVadocz <viliam.vadocz@gmail.com>
2019-07-30 20:32:27 +0200

    Added new features

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 User guide:
 
 - Download this repo.
-- Make sure main.py is in the same directory as the Leaderboard_empty.png, the font .tff file, and the two txt files with the standings.
+- Make sure that:
+    - main.py is in the same directory as the Leaderboard_empty.png, the font .tff file, and the two txt files with the standings.
+    - there is a subdirectory called emblems with the emblems for each division. (size 300x300 png)
 - Copy and paste the previous week's and current week's standings in the previous_week.txt and current_week.txt files.
     - The bot names should be seperated by new lines. You should be able to just copy and paste them directly from Google sheets.
 - Run main.py
-
-I still have some features I want to add, so stay tuned.
 
 Feel free to leave feedback. Enjoy!
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
 
-# TODO Add this to README.md
-# Bot names in the txts should be seperated by newline characters.
-# This should make it easy to just copy and paste from the sheet.
-
 # Getting previous week's standings from text file.
 previous_txt = open('previous_week.txt', 'r')
 previous = []
@@ -18,13 +14,14 @@ for line in current_txt:
     current.append(line.strip())
 current_txt.close
 
+# Creates lists to track which bots moved or which are new.
 new_bots = []
 moved_up = []
 moved_down = []
 
 # Loops through each bot in current standings.
 for bot in current:
-    # Finds if the bot is newly added.
+    # Finds if the bot is new.
     if bot not in previous:
         new_bots.append(bot)
 
@@ -39,17 +36,25 @@ for bot in current:
 divisions = ('Quantum', 'Overclocked', 'Processor', 'Circuit', 'Transistor', 'Abacus', 'Babbage', 'Colossus', 'Dragon', 'ENIAC')
 
 # Start positions for drawing.
-start_x = 450
+start_x = 540
 start_y = 90
 
+# Division emblem offsets from the division name position.
+emb_x_offset = -530
+emb_y_offset = -90
+
 # Bot name offsets from the division name position.
-x_offset = -220
-y_offset = 210
+bot_x_offset = -340
+bot_y_offset = 210
+
+# Offsets for the symbols from the bot name position.
+sym_x_offset = 1300
+sym_y_offset = 0
 
 # Incremenets for x and y.
-div_x_i = 1850
-div_y_i = 790
-bot_y_i = 140
+div_x_incr = 1790
+div_y_incr = 790
+bot_y_incr = 140
 
 # Opening image for drawing.
 image = Image.open('Leaderboard_empty.png')
@@ -65,20 +70,49 @@ bot_colour = (0,0,0)
 
 # For each divion, draw the division name, and each bot in the division.
 for i, div in enumerate(divisions):
+
     # Calculates position for the division text.
-    div_pos = (start_x + div_x_i * (i // 5), start_y + div_y_i * (i % 5))
+    div_pos = (start_x + div_x_incr * (i // 5), start_y + div_y_incr * (i % 5))
     # Draws the division name.
     draw.text(xy=div_pos, text=div, fill=div_colour, font=div_font)
 
+    # Draws the division emblem at an offset.
+    try:
+        # Opens the divicion emblem image.
+        emblem = Image.open(f'emblems/{div}.png')
+        # Calculates position of emblem.
+        emb_pos = (div_pos[0] + emb_x_offset, div_pos[1] + emb_y_offset)
+        # Pastes emblem onto image.
+        image.paste(emblem, emb_pos, emblem)
+    except:
+        # Sends error message if it can't find the emblem.
+        print(f'ERROR: Missing emblem for {div}.')
+
     # Loops through the four bots in the division and draws each.
-    bot_i = i * 4
-    for ii, bot in enumerate(current[bot_i : bot_i+4]):
+    bot_index = i * 4
+    for ii, bot in enumerate(current[bot_index : bot_index+4]):
+
         # Calculates position for the bot.
-        bot_pos = (div_pos[0] + x_offset, div_pos[1] + y_offset + ii * bot_y_i)
+        bot_pos = (div_pos[0] + bot_x_offset, div_pos[1] + bot_y_offset + ii * bot_y_incr)
         # Draws the bot name.
         draw.text(xy= bot_pos, text=bot, fill=bot_colour, font=bot_font)
 
-        # TODO Add symbols for new bot, moving up, moving down.
+        # Calculates symbol position.
+        sym_pos = (bot_pos[0] + sym_x_offset, bot_pos[1] + sym_y_offset)
+
+        # Pastes appropriate symbol
+        if bot in new_bots:
+            symbol = Image.open('symbols/new.png')
+            image.paste(symbol,sym_pos,symbol)
+
+        elif bot in moved_up:
+            symbol = Image.open('symbols/up.png')
+            image.paste(symbol,sym_pos,symbol)
+
+        elif bot in moved_down:
+            symbol = Image.open('symbols/down.png')
+            image.paste(symbol,sym_pos,symbol)
+
 
 # Shows and saves the image.
 image.show()
